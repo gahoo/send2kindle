73264c6a592ffd7bd9adab856a344b7cbd44d8ac
Gahoolee <gahoolee@gmail.com>
2018-09-30 17:35:52 +0800

    fix html content encoding issue by adding <head>; add option to choose save img or not;

--- url2html.py
+++ url2html.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 
+import requests
 import os
 import sys
 import argparse
@@ -7,20 +8,33 @@ import pdb
 from readability import Document
 from subprocess import call
 from webpage2html import generate
+from lxml import etree
 
-def saveHTML(url, prefix=''):
-    text = generate(url)
+def fixHeader(clean_html):
+    nodes = etree.fromstring(clean_html)
+    head_node = etree.fromstring('<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta></head>')
+    nodes.insert(0, head_node)
+    return etree.tostring(nodes)
+
+def saveHTML(url, img=True, prefix=''):
+    if img:
+        text = generate(url)
+    else:
+        response = requests.get(url)
+        text = response.text
     doc = Document(text)
+    content = fixHeader(doc.summary())
+
     filename = os.path.join(prefix, doc.title() + '.html')
     with open(filename, 'w') as f:
-        f.write(doc.summary().encode('utf-8'))
+        f.write(content)
     return filename
 
 def html2mobi(filename):
     call(["./kindlegen", filename, '-gif', '-locale', 'zh'])
 
 def main(args):
-    html_file = saveHTML(args.url, args.prefix)
+    html_file = saveHTML(args.url, args.img, args.prefix)
     if args.mobi:
         html2mobi(html_file)
 
@@ -32,6 +46,7 @@ if __name__ == '__main__':
     parsers.add_argument('--url', help = "url you want to save.")
     parsers.add_argument('--prefix', default='', help = "output prefix dir.")
     parsers.add_argument('--mobi', action="store_true", default=False, help = "convert to mobi or not.")
+    parsers.add_argument('--img', action="store_true", default=False, help = "save image.")
     parsers.set_defaults(func=main)
 
     argslist = sys.argv[1:]
